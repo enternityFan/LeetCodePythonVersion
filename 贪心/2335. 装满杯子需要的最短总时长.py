7277e0c8541bc82bac69707e2c8661afd910201a
enternityFan <51934710+enternityFan@users.noreply.github.com>
2022-09-18 12:00:29 +0800

    0918 第十一天。

--- 贪心/2335. 装满杯子需要的最短总时长.py
+++ 贪心/2335. 装满杯子需要的最短总时长.py
@@ -3,3 +3,24 @@
 # @File : 2335. 装满杯子需要的最短总时长.py
 # @Software: PyCharm
 # C'est la vie,enjoy it! :D
+from typing import List
+
+
+class Solution:
+    def fillCups(self, amount: List[int]) -> int:
+        ans = 0
+        n = len(amount)
+        while amount[0] >0 or amount[1] > 0 or amount[2] > 0:
+            amount.sort()
+            if amount[1] > 0 and amount[2] > 0:
+                amount[1] -=1
+                amount[2] -=1
+            else:
+                amount[2] -=1
+
+            ans +=1
+
+
+
+
+        return ans
