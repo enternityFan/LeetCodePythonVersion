e6180ba2338f8aead92284727378591c5b5007be
enternityFan <51934710+enternityFan@users.noreply.github.com>
2022-09-27 08:36:29 +0800

    0926 第十六天。

--- 位运算/面试题 17.19. 消失的两个数字.py
+++ 位运算/面试题 17.19. 消失的两个数字.py
@@ -1,10 +1,38 @@
-# @Time : 2022-09-26 11:57
+# @Time : 2022-09-26 12:12
 # @Author : Phalange
 # @File : 面试题 17.19. 消失的两个数字.py
 # @Software: PyCharm
 # C'est la vie,enjoy it! :D
-
+from typing import List
 
 
 class Solution:
-    def missingTwo(self, nums: List[int]) -> List[int]:
+   """
+   a + b = c
+   a * b = d
+   a * (c - a) = d
+   """
+   def missingTwo(self, nums: List[int]) -> List[int]:
+        x = 0
+        n = len(nums)
+
+        for a in nums:
+            x ^= a
+
+        for b in range(1, n + 2 + 1):
+            x ^= b
+
+
+        lb = x & (-x)
+
+        y = 0
+        for a in nums:
+            if a & lb:
+                y ^= a
+
+        for b in range(1, n + 2 + 1):
+            if b & lb:
+                y ^= b
+
+        return [y, y ^ x]
+
