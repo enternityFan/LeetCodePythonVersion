13cf09b5503bdeb5ca74a8d7d7ed65c479c7a2d1
enternityFan <51934710+enternityFan@users.noreply.github.com>
2022-10-18 12:54:10 +0800

    1018 第二十七天。

---
+++ 计数/1419. 数青蛙.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@Project ：LeetCodePthonVersion 
+@File ：1419. 数青蛙.py
+@Author ：HuntingGame
+@Date ：2022-10-18 12:52 
+C'est la vie!!! enjoy ur day :D
+'''
+
+
+class Solution:
+    def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
+        ans = 0
