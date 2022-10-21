ab5a110fb1fe6d846dd899bbb9b977e2dd6a0653
enternityFan <51934710+enternityFan@users.noreply.github.com>
2022-10-21 08:55:41 +0800

    1021 第三十天。

---
+++ 单调栈/901. 股票价格跨度.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@Project ：LeetCodePthonVersion 
+@File ：901. 股票价格跨度.py
+@Author ：HuntingGame
+@Date ：2022-10-21 8:44 
+C'est la vie!!! enjoy ur day :D
+'''
+from math import inf
+
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = [(-1, inf)]
+        self.idx = -1
+
+    def next(self, price: int) -> int:
+        self.idx +=1
+        while price >=self.stack[-1][1]:
+            self.stack.pop()
+        self.stack.append((self.idx,price))
+        return self.idx - self.stack[-2][0]
+
+
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
